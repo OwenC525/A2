77f6951cbdabf20b4c43da04a8466a6106006083
Owen Chiu <owencc8@gmail.com>
2025-09-25 21:06:57 -0400

    initial

---
+++ RoadTrip.java
@@ -0,0 +1,21 @@
+/*************************************************************************
+ * Compilation:  javac RoadTrip.java
+ * Execution:    java RoadTrip <gallons> <mpg> <isRaining> <hasMountainPass> <numPassengers> <isWeekend>
+ *
+ * Estimates the maximum driving range of a car based on fuel, fuel efficiency,
+ * and several trip conditions
+ *
+ * @author Matthew Specht
+ *************************************************************************/
+public class RoadTrip {
+
+    /**
+     * Parses command-line arguments, applies modifiers, and prints
+     * the expected driving range in miles (rounded to one decimal place) <- may need to be changed, depends if this has been taught at this point in CS111
+     *
+     * @param args six command-line arguments: gallons mpg isRaining hasMountainPass numPassengers isWeekend
+     */
+    public static void main(String[] args) {
+        //WRITE YOUR CODE HERE
+    }
+}
---
+++ Welcome.java
@@ -0,0 +1,5 @@
+public class Welcome {
+    public static void main(String[] args) {
+        // WRITE YOUR CODE HERE
+    }
+}
