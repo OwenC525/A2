f88b2a7643dea75c6ec11397c149d195d90ec985
Owen Chiu <owencc8@gmail.com>
2025-09-26 00:46:25 -0400

    road trip done, a2 done

--- RoadTrip.java
+++ RoadTrip.java
@@ -16,6 +16,30 @@ public class RoadTrip {
      * @param args six command-line arguments: gallons mpg isRaining hasMountainPass numPassengers isWeekend
      */
     public static void main(String[] args) {
-        //WRITE YOUR CODE HERE
+        double gallons = Double.parseDouble(args[0]);
+        double mpg = Double.parseDouble(args[1]);
+        boolean isRaining = Boolean.parseBoolean(args[2]);
+        boolean hasMountainPass = Boolean.parseBoolean(args[3]);
+        int numPassengers = Integer.parseInt(args[4]);
+        boolean isWeekend = Boolean.parseBoolean(args[5]);
+
+        if (isWeekend == true) {
+            mpg = mpg * 0.70;
+        }
+
+        if (isRaining == true) {
+            mpg = mpg * 0.85;
+        }
+
+        if (hasMountainPass == true) {
+            mpg = mpg * 0.90;
+        }
+
+        if (numPassengers >= 3) {
+            mpg = mpg * 1.05;
+        }
+
+        double distance = gallons * mpg;
+        System.out.println(distance + " miles");
     }
 }
